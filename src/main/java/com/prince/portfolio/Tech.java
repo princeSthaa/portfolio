b09bd1521954e657a899fa9513a3c3bf02fdbca9
oidex69 <shresthaprince222@gmail.com>
2026-01-13 23:00:24 +0545

    Changes in project class

---
+++ src/main/java/com/prince/portfolio/Tech.java
@@ -0,0 +1,18 @@
+package com.prince.portfolio;
+
+public class Tech {
+    private String name;
+    private String icon; // image path
+
+    public String getName() { return name; }
+
+    public String getIcon() { return icon; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
--- src/main/java/com/prince/portfolio/project.java
+++ src/main/java/com/prince/portfolio/project.java
@@ -1,8 +1,15 @@
 package com.prince.portfolio;
 
+import java.util.List;
+
 public class project {
     private String name;
     private String path;
+    private String slug;
+    private String image;
+    private String description;
+    private List<Tech> techStack;
+
 
     public project(String name, String path) {
         this.name = name;
